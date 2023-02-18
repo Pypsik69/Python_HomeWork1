d1192962e139919a6b28c88a7d6d526f2e82738c
Pypsik69 <matan.sula@gmail.com>
2023-02-18 17:46:05 +0600

    test commit

--- task1.py
+++ task1.py
@@ -3,7 +3,7 @@
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
 
-input_number = int(input("Введите число: "))
+input_number = int(input("Введите трёхзначное число: "))
 first_number = input_number % 10
 last_two_numbers = input_number // 10
 second_number = last_two_numbers % 10
